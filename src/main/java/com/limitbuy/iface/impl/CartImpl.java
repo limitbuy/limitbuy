3e4caacda06e41c9068460cefec024a0268c125e
肖龙雾 <longwu.xiao@ele.me>
2015-10-28 18:09:51 +0800

    commit

---
+++ src/main/java/com/limitbuy/iface/impl/CartImpl.java
@@ -0,0 +1,21 @@
+package com.limitbuy.iface.impl;
+
+import com.limitbuy.entity.Cart;
+import com.limitbuy.iface.CartInterface;
+
+/**
+ * Created by longwu on 15/10/28.
+ */
+public class CartImpl implements CartInterface{
+    public String addToCart(Cart cart) {
+        return null;
+    }
+
+    public String deleteFromCart(String productId) {
+        return null;
+    }
+
+    public String cart() {
+        return null;
+    }
+}
---
+++ src/main/java/com/limitbuy/iface/impl/OrderImpl.java
@@ -0,0 +1,17 @@
+package com.limitbuy.iface.impl;
+
+import com.limitbuy.entity.Order;
+import com.limitbuy.iface.OrderInterface;
+
+/**
+ * Created by longwu on 15/10/28.
+ */
+public class OrderImpl implements OrderInterface {
+    public String orderPay(Order order) {
+        return null;
+    }
+
+    public String orderCancel(String orderId) {
+        return null;
+    }
+}
